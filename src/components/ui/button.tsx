bae936abdb5f93a50cbbb46548eba27222127d9c
Sonu Kumar <sonukumar7004923959@gmail.com>
2025-11-05 01:52:46 +0530

    G

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -52,8 +52,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         disabled={loading || props.disabled}
         {...props}
       >
-        {loading && <Loader2 className="h-4 w-4 animate-spin" />}
-        {children}
+        {asChild ? children : (
+          <>
+            {loading && <Loader2 className="h-4 w-4 animate-spin" />}
+            {children}
+          </>
+        )}
       </Comp>
     )
   }
